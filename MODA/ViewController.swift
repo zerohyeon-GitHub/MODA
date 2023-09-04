ec80e980ac480d156895acbd8a23f1ec233cce18
Kyle <kyle.pxrk@gmail.com>
2023-09-04 16:42:38 +0900

    Add: gitignore

---
+++ MODA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MODA
+//
+//  Created by Kyle on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
