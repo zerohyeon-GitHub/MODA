4323f01c8568295c23db3a79af94b19696df36fc
june-hehe <sdj01071242989@gmail.com>
2023-09-05 09:54:41 +0900

    MOD: 기본 UI구현중

---
+++ MODA/View/MyPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageView.swift
+//  MODA
+//
+//  Created by DJ S on 2023/09/05.
+//
+
+import UIKit
+
+class MyPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
